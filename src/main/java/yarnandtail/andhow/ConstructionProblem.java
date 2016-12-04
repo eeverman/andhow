11af1765fac8ffc0cddee37a415bb8dab3bd4fd5
eeverman <ericeverman@Rebeccas-MacBook-Air.local>
2016-12-03 23:32:00 -0600

    update from master

---
+++ src/main/java/yarnandtail/andhow/ConstructionProblem.java
@@ -0,0 +1,12 @@
+package yarnandtail.andhow;
+
+/**
+ * A problem building up the basic AppConfiguration, prior to attempting to load
+ * any values.
+ * 
+ * 
+ * @author ericeverman
+ */
+public class ConstructionProblem {
+	
+}
