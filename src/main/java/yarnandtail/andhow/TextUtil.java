5d8e5f42be33ccddfce6d1112feb7e3e13f48bf6
eeverman <ericeverman@Rebeccas-MacBook-Air.local>
2016-12-06 20:58:23 -0600

    New TestUtil class to simplify message formatting

---
+++ src/main/java/yarnandtail/andhow/TextUtil.java
@@ -0,0 +1,73 @@
+package yarnandtail.andhow;
+
+import java.io.PrintStream;
+
+/**
+ * Text utility used for formatting output for reports, auto generated descriptions
+ * and Problems.
+ * 
+ * @author ericeverman
+ */
+public class TextUtil {
+	
+	private static final String PAD = "[[00PAD00]]";
+	
+	/**
+	 * println to the passed PrintStream using the format {} patterning.
+	 * @param out
+	 * @param pattern
+	 * @param args 
+	 */
+	public static void println(PrintStream out, String pattern, Object... args) {
+		out.println(format(pattern, args));
+	}
+	
+	/**
+	 * print to the passed PrintStream using the format {} patterning.
+	 * @param out
+	 * @param pattern
+	 * @param args 
+	 */
+	public static void print(PrintStream out, String pattern, Object... args) {
+		out.print(format(pattern, args));
+	}
+	
+	/**
+	 * Creates a message with {} instances replaced with values from the args
+	 * list, similar to how SLF4J formats messages.
+	 * 
+	 * LIMITATION:  \{} is recognized as escaping the {}, however, \\{} is not
+	 * correctly recognized as preceding the {} with a '\' character.  In other
+	 * words, the sequence '\{}' is the only exception that is looked for /
+	 * handled.
+	 * 
+	 * @param pattern A string pattern with may contain {} references to items in the args list.
+	 * @param args Values to call .toString on to fill in, in order, the {} refs.
+	 * @return 
+	 */
+	public static String format(String pattern, Object... args) {
+		
+		//Its ambiguous after we split into tokens if there are start or end {}'s.
+		//Adding padding solves the issue.
+		String padded = PAD + pattern + PAD;
+		
+		//This regex starts with a negative backreference (?<!\\) meaning that
+		//the matched expression must not follow the backslash literal.
+		String[] tokens = padded.split("(?<!\\\\)(\\{})");
+		
+		if (tokens.length > 1) {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append(tokens[0]);
+			
+			for (int i = 1; i < tokens.length; i++) {
+				sb.append(args[i - 1]);
+				sb.append(tokens[i]);
+			}
+			
+			return sb.substring(11, sb.length() - 11);
+		} else {
+			return pattern;
+		}
+	}
+}
---
+++ src/test/java/yarnandtail/andhow/TextUtilTest.java
@@ -0,0 +1,64 @@
+package yarnandtail.andhow;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author ericeverman
+ */
+public class TextUtilTest {
+	ByteArrayOutputStream baos;
+	PrintStream ps;
+	
+	@Before
+	public void setup() {
+		baos = new ByteArrayOutputStream();
+		ps = new PrintStream(baos) {
+			
+			//Hack to prevent newlines at the end of the lines to make testing easier
+			@Override
+			public void println(Object x) {
+				super.print(x);
+			}
+			
+			@Override
+			public void println(String x) {
+				super.print(x);
+			}
+			
+		};
+	}
+	
+	protected String getStreamContent() {
+		return new String(baos.toByteArray(), StandardCharsets.UTF_8);
+	}
+	
+	@Test
+	public void testFormat() {
+		assertEquals("abcXXXxyz", TextUtil.format("abc{}xyz", "XXX"));
+		assertEquals("AAAabcBBBxyzCCC", TextUtil.format("{}abc{}xyz{}", "AAA", "BBB", "CCC"));
+		assertEquals("AAAabcBBBxyzCCC", TextUtil.format("{}abc{}xyz{}", "AAA", "BBB", "CCC"));
+		TextUtil.format("abc\\{}xyz", "XXX");
+		assertEquals("abc\\{}xyz", TextUtil.format("abc\\{}xyz", "XXX"));
+	}
+
+
+	@Test
+	public void testSimplePatternsPrintln() {
+		TextUtil.println(ps, "abc{}xyz", "XXX");
+		assertEquals("abcXXXxyz", getStreamContent());
+		
+	}
+	
+	@Test
+	public void testPatternsWithCurlyBraceLiteralsPrintln() {
+		TextUtil.println(ps, "abc\\{}xyz", "XXX");
+		assertEquals("abc\\{}xyz", getStreamContent());
+	}
+	
+}
