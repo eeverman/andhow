9678b842ab3c20af39f26462ee536ce84a5050c3
eeverman <ericeverman@Rebeccas-MacBook-Air.local>
2016-12-07 22:49:16 -0600

    report text improvement

--- src/main/java/yarnandtail/andhow/ReportGenerator.java
+++ src/main/java/yarnandtail/andhow/ReportGenerator.java
@@ -14,7 +14,7 @@ public class ReportGenerator {
 	
 	public static final String ANDHOW_NAME = "AndHow!";
 	public static final String ANDHOW_URL = "https://github.com/eeverman/andhow";
-	public static final String ANDHOW_TAG_LINE = "Simple-Strong-Valid App Configuration";
+	public static final String ANDHOW_TAG_LINE = "simple.strong.valid.App_Configuration";
 	
 	public static void printProblems(PrintStream out, AppFatalException fatalException, AppConfigDefinition appDef) {
 		
