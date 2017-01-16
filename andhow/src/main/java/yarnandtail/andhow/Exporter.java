b5c6e0ae6210532ce5a189b5819bc7217ca176db
eeverman <ericeverman@Rebeccas-MacBook-Air.local>
2017-01-15 21:43:07 -0600

    implementation start

---
+++ andhow/src/main/java/yarnandtail/andhow/Exporter.java
@@ -0,0 +1,52 @@
+package yarnandtail.andhow;
+
+/**
+ * The Exporter provides support for legacy applications that expect
+ * to find key/value pairs in a specific source, such as system properties.
+ * 
+ * This allows applications to use AndHow for configuration and still
+ * maintain compatibility with legacy modules/libraries.  New code can begin to
+ * use the direct Property access that AndHow provides.
+ * 
+ * Exporting is considered 'safe', so overlapping export configuration, such
+ * as application level and PropertyGroup level are read permissively.
+ * 
+ * 
+ * @author ericeverman
+ */
+public interface Exporter {
+	
+
+	/**
+	 * Exports all properties.
+	 * 
+	 * Based on its configuration, an exporter can decide which
+	 * properties should be exported and what name or aliases should be used
+	 * when exporting.
+	 * 
+	 * @param definition
+	 * @param values
+	 */
+	void export(ConstructionDefinition definition, ValueMap values);
+	
+	/**
+	 * Exports a PropertyGroup.
+	 * 
+	 * Based on its configuration, an exporter can decide which
+	 * properties should be exported and what name or aliases should be used
+	 * when exporting.
+	 * 
+	 * @param group
+	 * @param definition
+	 * @param values
+	 */
+	void export(PropertyGroup group, ConstructionDefinition definition, ValueMap values);
+	
+	public static enum INCLUDE_CANONICAL_NAMES {
+		ALL, ONLY_IF_NO_OUT_ALIAS, NONE;
+	}
+	
+	public static enum INCLUDE_OUT_ALIAS_NAMES {
+		ALL, NONE;
+	}
+}
---
+++ andhow/src/main/java/yarnandtail/andhow/export/SysPropExporter.java
@@ -0,0 +1,17 @@
+package yarnandtail.andhow.export;
+
+import yarnandtail.andhow.*;
+
+public class SysPropExporter implements Exporter {
+
+	@Override
+	public void export(ConstructionDefinition definition, ValueMap values) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void export(PropertyGroup group, ConstructionDefinition definition, ValueMap values) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
