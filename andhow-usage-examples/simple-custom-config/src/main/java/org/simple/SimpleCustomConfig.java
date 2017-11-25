a561b32ec3a3976ef159b2c9611f6e35bbe5f269
Eric Everman <eeverman@gmail.com>
2017-11-24 21:55:17 -0600

    Now have a single working test using auto-discovered configuration

--- andhow-usage-examples/simple-custom-config/src/main/java/org/simple/SimpleCustomConfig.java
+++ andhow-usage-examples/simple-custom-config/src/main/java/org/simple/SimpleCustomConfig.java
@@ -1,23 +1,17 @@
 package org.simple;
 
-/**
- * This example has almost no comments b/c it's a minimal example for the AndHow! homepage.
- */
 import org.yarnandtail.andhow.property.*;
 import org.yarnandtail.andhow.*;
 
-//1
 @GroupInfo(name="Simple AndHow usage example", desc="@GroupInfo documents the contained Properties")
-public class GettingStarted {
-	//2
+public class SimpleCustomConfig {
+
 	final static IntProp COUNT_DOWN_START = IntProp.builder().mustBeNonNull().
 			mustBeGreaterThanOrEqualTo(1).defaultValue(2).build();
 	
-	//3
 	private final static StrProp LAUNCH_COMMAND = StrProp.builder().mustBeNonNull().
 			mustMatchRegex(".*Go.*").build();
 	
-	//4
 	public String launch() {
 		String launch = "";
 		
