5519b3687d30f2a4d55ff618836a8a79b75e25a6
Eric Everman <eeverman@gmail.com>
2018-08-11 23:42:37 -0500

    Updated javadoc

--- andhow-core/src/main/java/org/yarnandtail/andhow/load/std/StdPropFileOnClasspathLoader.java
+++ andhow-core/src/main/java/org/yarnandtail/andhow/load/std/StdPropFileOnClasspathLoader.java
@@ -22,61 +22,54 @@ import org.yarnandtail.andhow.load.PropFileOnClasspathLoader;
  * <em>Property value loading is based on a 'first win' strategy, so the first
  * loader to find a value for a property sets the value.</em>
  * <h4>Typical Use Case</h4>
- * A typical service application pulls in the majority of its configuration from
- * system properties, environmental variables and JNDI.  The application has some
- * configuration settings that need sane defaults, so an {@code andhow.properties}
- * file is included with the application at the root of the classpath.
- * With no additional configuration, AndHow will discover the file and load its
- * property values via the {@code StdPropFileOnClasspathLoader}.
+ * A service application might load the majority of its configuration from
+ * system properties or environmental variables, however, some sane default
+ * configuration values can be bundled with the application.
+ * By default, AndHow will discover and load a file named {@code andhow.properties}
+ * at the root of the classpath via the {@code StdPropFileOnClasspathLoader}.
  * <h4>Basic Behaviors</h4>
  * <ul>
  * <li><b>Pre-trims String values: No</b> (Individual Properties may still trim values)
  * <li><b>Complains about unrecognized properties: Yes</b>
  * <li><b>Complains if the .properties file is missing: No</b>
+ * <li><b>Default behavior:  Attempts to read {@code andhow.properties} from the root of the classpath</b>
  * </ul>
  * <h4>Other Loader Details</h4>
- * This loader and all properties file based loaders use
- * {@code java.util.Properties} to parse properties files, so several behaviors
- * are determined by that class.
- * First, {@code Properties} silently ignores duplicate property entries
- * (i.e., the same key value appearing multiple times). In this situation,
- * only the last of the property values in the file is used.
- * Second, key and value delimiters, line endings and line continuation are 
- * determined according to the
+ * This loader reads properties files using the {@code java.util.Properties},
+ * which silently ignores duplicate property entries
+ * (i.e., the same key appearing multiple times).  When there are duplicate 
+ * property keys in a properties file, only the last assigned value is used.
+ * Full details on how Java parses properties files can be found in the
  * <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Properties.html#load-java.io.Reader-">properties file specification</a>.
  * <p>
- * If a property file location other than the default ({@code /andhow.properties})
- * is needed, a new location can be configured like this:
+ * Configuring the name or classpath of the properties file can be used to
+ * enable different configuration profiles based on the environment.
+ * For instance, a system property could specify that {@code /test.properties}
+ * be used on a test server and {@code /production.properties} on a production
+ * server.  An example of configuring the property file path:
  * <pre>
  * import org.yarnandtail.andhow.*;
  * import org.yarnandtail.andhow.property.StrProp;
  * 
  * public class UsePropertyFileOnClasspath implements AndHowInit {
- *   public static final StrProp MY_PROP_FILE_ON_CLASSPATH = StrProp.builder()
+ *   public static final StrProp MY_CLASSPATH = StrProp.builder()
  *	   .desc("Path to a properties file on the classpath. "
  *       + "If the file is not present, it is not considered an error.").build();
  * 
  *   {@literal @}Override
  *   public AndHowConfiguration getConfiguration() {
  *     return  StdConfig.instance()
- *       .setClasspathPropFilePath(MY_PROP_FILE_ON_CLASSPATH);
+ *       .setClasspathPropFilePath(MY_CLASSPATH);
  *   }
  * }
  * </pre>
- * The code above adds a String Property named {@code MY_PROP_FILE_ON_CLASSPATH}
- * (the name is arbitrary)
- * which is used to tell {@code AndHow} where to find a properties file
- * on the classpath.  When {@code AndHow} initializes, the
- * {@code StdPropFileOnClasspathLoader} checks to see if a value has been loaded
- * for {@code MY_PROP_FILE_ON_CLASSPATH} by a prior loader.  If a value is present,
- * the loader tries to load from the configured classpath.  If no value is
- * configured, a default classpath of {@code /andhow.properties} is assumed.
- * <br>
- * Adding a {@code Property} to enable a custom classpath properties file can be useful
- * for testing or common deployment profiles.  For instance, a system property
- * could specify that {@code /test.properties} be used during testing,
- * {@code /qa_mode.properties} on a QA server and {@code /production.properties} on
- * a production server.
+ *  The code above adds the property {@code MY_CLASSPATH}
+ * (the name is arbitrary) which is used to configure the 
+ * {@code StdPropFileOnClasspathLoader} with a custom property file location.
+ * When AndHow initializes, the {@code StdPropFileOnClasspathLoader} checks to
+ * see if a value has been loaded for {@code MY_CLASSPATH} by any prior loader.
+ * If a value is present, the loader tries to load from the configured classpath.
+ * If no value is configured, the default classpath is assumed.
  * 
  * <h4>This is a Standard Loader</h4>
  * Like all {@code StandardLoader}'s, this loader is intended to be auto-created
