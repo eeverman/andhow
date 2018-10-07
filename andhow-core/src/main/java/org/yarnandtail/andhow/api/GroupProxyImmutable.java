6e9f649f81604679b6ca3add97abb9ba842e4a87
Eric <eeverman@users.noreply.github.com>
2018-10-07 17:34:19 -0500

    Some constructors were unused and were removed (#365)

--- andhow-core/src/main/java/org/yarnandtail/andhow/api/GroupProxyImmutable.java
+++ andhow-core/src/main/java/org/yarnandtail/andhow/api/GroupProxyImmutable.java
@@ -4,7 +4,8 @@ import java.util.*;
 import org.yarnandtail.andhow.internal.NameAndProperty;
 
 /**
- *
+ * A non-modifiable GroupProxy.
+ * 
  * @author ericeverman
  */
 public class GroupProxyImmutable extends GroupProxyBase {
@@ -12,14 +13,12 @@ public class GroupProxyImmutable extends GroupProxyBase {
 	/**
 	 * Constructs a new instance w/ final unmodifiable fields.
 	 * 
-	 * @param canonicalName
-	 * @param javaCanonicalName
-	 * @param props
+	 * @param canonicalName AndHow name of the group
+	 * @param javaCanonicalName Java name of the group (inner classes use a $ instead of a dot)
+	 * @param props A list of contained Properties
+	 * @param userGroup If true, this is a typical group.
+	 *   If false, it is an internal property use to configure AndHow.
 	 */
-	public GroupProxyImmutable(String canonicalName, String javaCanonicalName, List<NameAndProperty> props) {
-		super(canonicalName, javaCanonicalName, props);
-	}
-	
 	public GroupProxyImmutable(String canonicalName, String javaCanonicalName, 
 			List<NameAndProperty> props, boolean userGroup) {
 		super(canonicalName, javaCanonicalName, props, userGroup);
--- andhow-core/src/main/java/org/yarnandtail/andhow/api/GroupProxyMutable.java
+++ andhow-core/src/main/java/org/yarnandtail/andhow/api/GroupProxyMutable.java
@@ -3,7 +3,8 @@ package org.yarnandtail.andhow.api;
 import org.yarnandtail.andhow.internal.NameAndProperty;
 
 /**
- *
+ * A modifiable GroupProxy.
+ * 
  * @author ericeverman
  */
 public class GroupProxyMutable extends GroupProxyBase {
@@ -12,16 +13,13 @@ public class GroupProxyMutable extends GroupProxyBase {
 	/**
 	 * Constructs a new instance w/ final unmodifiable fields.
 	 * 
-	 * @param canonicalName
-	 * @param javaCanonicalName
+	 * @param canonicalName The AndHow name
+	 * @param javaCanonicalName Java name of the group (inner classes use a $ instead of a dot)
 	 */
 	public GroupProxyMutable(String canonicalName, String javaCanonicalName) {
 		super(canonicalName, javaCanonicalName);
 	}
 	
-	public GroupProxyMutable(String canonicalName, String javaCanonicalName, boolean userGroup) {
-		super(canonicalName, javaCanonicalName, userGroup);
-	}
 	
 	public void addProperty(NameAndProperty prop) {
 		props.add(prop);
