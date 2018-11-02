c2db61df6e61d9f7a906e076e496a6f40ab5dec6
Eric Everman <eeverman@gmail.com>
2018-11-01 21:47:56 -0500

    Implemented Chace’s suggestion Added javadocs to all my code additions and more.

--- andhow-annotation-processor/src/main/java/org/yarnandtail/andhow/compile/CompileProblem.java
+++ andhow-annotation-processor/src/main/java/org/yarnandtail/andhow/compile/CompileProblem.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 import org.yarnandtail.andhow.util.TextUtil;
 
 /**
- *
- * @author ericevermanpersonal
+ * Implementation represent specific problems with AndHow Property usage or
+ * initiation, discovered during compilation.
  */
 public abstract class CompileProblem {
 	
@@ -15,30 +15,65 @@ public abstract class CompileProblem {
 	final String propName;
 	final boolean propProblem;
 	
+	/**
+	 * Base constructor for Property related problems.
+	 * 
+	 * @param groupName Full name of the class containing the {@code Property}.
+	 * If an inner class, should be the complete name w/ the inner class.
+	 * @param propName The declared name of the {@code Property} w/ the problem.
+	 */
 	CompileProblem(String groupName, String propName) {
 		this.groupName = groupName;
 		this.propName = propName;
 		propProblem = true;
 	}
 		
+	/**
+	 * Base constructor for non-Property related problems, such as Initiation
+	 * errors.
+	 */
 	CompileProblem() {
 		groupName = null;
 		propName = null;
 		propProblem = false;
 	}
 	
+	/**
+	 * Full name of the class containing the {@code Property}.
+	 * In the case of an inner class, this should be the complete name including
+	 * that inner class.
+	 * 
+	 * @return May be null if this is not a property related problem.
+	 */
 	public String getGroupName() {
 		return groupName;
 	}
 	
+	/**
+	 * The declared name of the {@code Property} that has the problem.
+	 * 
+	 * @return May be null if this is not a property related problem.
+	 */
 	public String getPropertyName() {
 		return propName;
 	}
 	
+	/**
+	 * If true, the problem is related to a specific {@code Property}.
+	 * @return 
+	 */
 	public boolean isPropertyProblem() {
 		return propProblem;
 	}
 	
+	/**
+	 * Logical equals implementation based on equality of fields.
+	 * 
+	 * This is only used for comparisons during testing.
+	 * 
+	 * @param o Compared to me.
+	 * @return True if logically equal.
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (o != null && this.getClass().isInstance(o)) {
@@ -53,10 +88,28 @@ public abstract class CompileProblem {
 		}
 	}
 	
+	/**
+	 * The complete message that will be logged during compilation.
+	 * 
+	 * Intended to be user readable w/ enough info for the user to understand
+	 * and find the issue.
+	 * 
+	 * @return A String explanation of the problem.
+	 */
 	public abstract String getFullMessage();
 	
+	/**
+	 * An AndHow Property is missing the {@code static} modifier.
+	 */
 	static class PropMissingStatic extends CompileProblem {
 		
+		/**
+		 * New instance.
+		 * 
+		 * @param groupName Full name of the class containing the {@code Property}.
+		 * If an inner class, should be the complete name w/ the inner class.
+		 * @param propName The declared name of the {@code Property} w/ the problem.
+		 */
 		public PropMissingStatic(String groupName, String propName) {
 			super(groupName, propName);
 		}
@@ -69,8 +122,18 @@ public abstract class CompileProblem {
 		}
 	}
 	
+	/**
+	 * An AndHow Property is missing the {@code final} modifier.
+	 */
 	static class PropMissingFinal extends CompileProblem {
 		
+		/**
+		 * New instance.
+		 * 
+		 * @param groupName Full name of the class containing the {@code Property}.
+		 * If an inner class, should be the complete name w/ the inner class.
+		 * @param propName The declared name of the {@code Property} w/ the problem.
+		 */
 		public PropMissingFinal(String groupName, String propName) {
 			super(groupName, propName);
 		}
@@ -83,8 +146,18 @@ public abstract class CompileProblem {
 		}
 	}
 	
+	/**
+	 * An AndHow Property is missing the {@code static} and {@code final} modifiers.
+	 */
 	static class PropMissingStaticFinal extends CompileProblem {
 		
+		/**
+		 * New instance.
+		 * 
+		 * @param groupName Full name of the class containing the {@code Property}.
+		 * If an inner class, should be the complete name w/ the inner class.
+		 * @param propName The declared name of the {@code Property} w/ the problem.
+		 */
 		public PropMissingStaticFinal(String groupName, String propName) {
 			super(groupName, propName);
 		}
@@ -97,10 +170,19 @@ public abstract class CompileProblem {
 		}
 	}
 	
+	/**
+	 * More than a single Init class was found on the classpath.
+	 */
 	static class TooManyInitClasses extends CompileProblem {
 		private final List<AndHowCompileProcessor.CauseEffect> _instances = new ArrayList();
 		private final String _fullInitClassName;
 		
+		/**
+		 * New instance.
+		 * 
+		 * @param fullInitClassName The Init interface name
+		 * @param instances  A list of instances that implement the interface.
+		 */
 		public TooManyInitClasses(
 				String fullInitClassName,
 				List<AndHowCompileProcessor.CauseEffect> instances) {
@@ -110,6 +192,11 @@ public abstract class CompileProblem {
 			
 		}
 		
+		/**
+		 * A list of full class names that implement the InitClassName interface.
+		 * 
+		 * @return A List of class names.  Never null.
+		 */
 		public List<String> getInstanceNames() {
 			List<String> names = new ArrayList();
 
@@ -120,6 +207,11 @@ public abstract class CompileProblem {
 			return names;
 		}
 		
+		/**
+		 * The Init interface name.
+		 * 
+		 * @return A class name.
+		 */
 		public String getInitClassName() {
 			return _fullInitClassName;
 		}
@@ -135,6 +227,14 @@ public abstract class CompileProblem {
 					_fullInitClassName, impList);
 		}
 		
+		/**
+		 * Logical equals implementation based on equality of fields.
+		 * 
+		 * This is only used for comparisons during testing.
+		 * 
+		 * @param o Compared to me.
+		 * @return True if logically equal.
+		 */
 		@Override
 		public boolean equals(Object o) {
 			if (o != null && this.getClass().isInstance(o)) {
