7d1ac6e26ea3070421189c937dfd7e582c622dff
Vicky Ronnen <vicky.ronnen@upcmail.nl>
2021-06-02 17:36:23 +0200

    Added conditional unwrapping of ProcessingEnvironment when running build in IntelliJ Idea 2020.3+

--- andhow-annotation-processor/src/main/java/org/yarnandtail/andhow/compile/AndHowCompileProcessor.java
+++ andhow-annotation-processor/src/main/java/org/yarnandtail/andhow/compile/AndHowCompileProcessor.java
@@ -9,20 +9,19 @@ import java.util.*;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
-import javax.tools.Diagnostic;
-
 
 import javax.tools.FileObject;
 import org.yarnandtail.andhow.AndHowInit;
 import org.yarnandtail.andhow.api.Property;
 import org.yarnandtail.andhow.service.*;
 
+import static javax.tools.Diagnostic.*;
 import static javax.tools.StandardLocation.CLASS_OUTPUT;
 import org.yarnandtail.andhow.util.TextUtil;
 
 /**
  * This is the central AndHow compilation class, an Annotation Processor.
- * 
+ *
  * An annotation processor nominally reads annotations as classes are compiled.
  * This class is annotated {@code SupportedAnnotationTypes("*")}, allowing it to
  * 'see' all classes as they are compiled.  This class then delegates to a
@@ -40,30 +39,36 @@ import org.yarnandtail.andhow.util.TextUtil;
  */
 @SupportedAnnotationTypes("*")
 public class AndHowCompileProcessor extends AbstractProcessor {
-	
+
 	private static final String INIT_CLASS_NAME = AndHowInit.class.getCanonicalName();
 	private static final String TEST_INIT_CLASS_NAME = "org.yarnandtail.andhow.AndHowTestInit";
-	
+
 	private static final String SERVICES_PACKAGE = "";
-	
+
 	private static final String SERVICE_REGISTRY_META_DIR = "META-INF/services/";
-	
+
 	//Static to insure all generated classes have the same timestamp
 	private static Calendar runDate;
-	
+
 	private final List<CauseEffect> registrars = new ArrayList();
-	
+
 	private final List<CauseEffect> initClasses = new ArrayList();		//List of init classes (should only ever be 1)
 	private final List<CauseEffect> testInitClasses = new ArrayList();	//List of test init classes (should only ever be 1)
 
 	private final List<CompileProblem> problems = new ArrayList();	//List of problems found. >0== RuntimeException
-	
+
 	/**
 	 * A no-arg constructor is required.
 	 */
 	public AndHowCompileProcessor() {
 		//used to ensure all metadata files have the same date
 		runDate = new GregorianCalendar();
+	}
+
+	@Override
+	public synchronized void init(ProcessingEnvironment processingEnv) {
+		//Unwrap the IntelliJ ProcessingEnvironment if needed
+		super.init(unwrap(processingEnv));
 	}
 
 	@Override
@@ -75,38 +80,35 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		
-		//TODO:  Unwrap the IntelliJ ProcessingEnvironment
-		ProcessingEnvironment unwrappedProcessingEnv = this.processingEnv;
-		
-		Filer filer = unwrappedProcessingEnv.getFiler();
-		Messager log = unwrappedProcessingEnv.getMessager();
-		
+
+		Filer filer = this.processingEnv.getFiler();
+		Messager log = this.processingEnv.getMessager();
+
 		boolean isLastRound = roundEnv.processingOver();
-		
+
 
 		if (isLastRound) {
 			debug(log, "Final round of annotation processing. Total root element count: {}",
 					roundEnv.getRootElements().size());
 
-			
+
 			if (initClasses.size() > 1) {
 				problems.add(new CompileProblem.TooManyInitClasses(
 						INIT_CLASS_NAME, initClasses));
 			}
-			
+
 			if (testInitClasses.size() > 1) {
 				problems.add(new CompileProblem.TooManyInitClasses(
 						TEST_INIT_CLASS_NAME, testInitClasses));
 			}
-			
+
 			if (problems.isEmpty()) {
 				try {
 					if (initClasses.size() == 1) {
 
 						debug(log, "Found exactly 1 {} class: {}",
 								INIT_CLASS_NAME, initClasses.get(0).fullClassName);
-						
+
 						writeServiceFile(filer, AndHowInit.class.getCanonicalName(), initClasses);
 
 					}
@@ -114,7 +116,7 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 					if (testInitClasses.size() == 1) {
 						debug(log, "Found exactly 1 {} class: {}",
 								TEST_INIT_CLASS_NAME, testInitClasses.get(0).fullClassName);
-						
+
 						writeServiceFile(filer, TEST_INIT_CLASS_NAME, testInitClasses);
 
 					}
@@ -122,7 +124,7 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 					if (registrars.size() > 0) {
 						writeServiceFile(filer, PropertyRegistrar.class.getCanonicalName(), registrars);
 					}
-					
+
 				} catch (IOException e) {
 					throw new AndHowCompileException("Exception while trying to write generated files", e);
 				}
@@ -131,14 +133,14 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 						+ "prevented compilation. Each of the following errors "
 						+ "must be fixed before compilation is possible.");
 				error(log, "AndHow errors discovered: {}", problems.size());
-				
+
 				for (CompileProblem err : problems) {
 					error(log, err.getFullMessage());
 				}
 
 				throw new AndHowCompileException(problems);
 			}
-			
+
 		} else {
 			debug(log, "Another round of annotation processing. "
 					+ "Current root element count: {}", roundEnv.getRootElements().size());
@@ -152,7 +154,7 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 
 				TypeElement te = (TypeElement) e;
 				AndHowElementScanner7 st = new AndHowElementScanner7(
-						unwrappedProcessingEnv,
+						this.processingEnv,
 						Property.class.getCanonicalName(),
 						INIT_CLASS_NAME,
 						TEST_INIT_CLASS_NAME);
@@ -167,9 +169,9 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 
 				if (ret.hasRegistrations()) {
 
-					debug(log, "Found {} AndHow Properties in class {} ", 
+					debug(log, "Found {} AndHow Properties in class {} ",
 							ret.getRegistrations().size(), ret.getRootCanonicalName());
-					
+
 					PropertyRegistrarClassGenerator gen = new PropertyRegistrarClassGenerator(ret, AndHowCompileProcessor.class, runDate);
 					registrars.add(new CauseEffect(gen.buildGeneratedClassFullName(), te));
 					PropertyRegistrationList regs = ret.getRegistrations();
@@ -182,7 +184,7 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 						throw new RuntimeException(ex);
 					}
 				}
-				
+
 				problems.addAll(ret.getProblems());
 
 			}
@@ -194,10 +196,10 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 
 	/**
 	 * Writes a new class implementing the {@code PropertyRegistrar} interface.
-	 * 
+	 *
 	 * The new class directly corresponds to a user classes containing AndHow
 	 * Properties and will contain meta data about the properties.
-	 * 
+	 *
 	 * @param filer The javac file system representation for writing files.
 	 * @param generator AndHow class capable of generating source code for this
 	 * {@code PropertyRegistrar} class.
@@ -207,11 +209,11 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 	 * so there is an association between the file and the reason it was written.
 	 * Likely this is normally used to associate source code line numbers with
 	 * generated code.
-	 * 
+	 *
 	 * @throws Exception If unable to write (out of disc space?)
 	 */
-	public void writeClassFile(Filer filer, 
-			PropertyRegistrarClassGenerator generator, Element causingElement) throws Exception {
+	public void writeClassFile(Filer filer,
+														 PropertyRegistrarClassGenerator generator, Element causingElement) throws Exception {
 
 		String classContent = generator.generateSource();
 
@@ -220,20 +222,20 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 
 		try (Writer writer = classFile.openWriter()) {
 			writer.write(classContent);
-		}	
+		}
 	}
-	
-	protected void writeServiceFile(Filer filer, 
-			String fullyQualifiedServiceInterfaceName, 
-			List<CauseEffect> implementingClasses) throws IOException {
-		
+
+	protected void writeServiceFile(Filer filer,
+																	String fullyQualifiedServiceInterfaceName,
+																	List<CauseEffect> implementingClasses) throws IOException {
+
 		//Get a unique causing elements
 		HashSet<Element> set = new HashSet();
 		for (CauseEffect ce : implementingClasses) {
 			set.add(ce.causeElement);
 		}
-		
-		
+
+
 		//The CLASS_OUTPUT location is used instead of SOURCE_OUTPUT because it
 		//seems that non-Java files are not copied over from the SOURCE_OUTPUT
 		//location.
@@ -247,36 +249,36 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 				writer.write(System.lineSeparator());
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Logs a debug message using the javac standard Messager system.
-	 * 
+	 *
 	 * @param log The Message instance to use
 	 * @param pattern String pattern with curly variable replacement like this: {}
 	 * @param args Arguments to put into the {}'s, in order.
 	 */
 	void debug(Messager log, String pattern, Object... args) {
-		log.printMessage(Diagnostic.Kind.NOTE, TextUtil.format(pattern, args));
+		log.printMessage(Kind.NOTE, TextUtil.format(pattern, args));
 	}
-	
+
 	/**
 	 * Logs an error message using the javac standard Messager system.
-	 * 
+	 *
 	 * @param log The Message instance to use
 	 * @param pattern String pattern with curly variable replacement like this: {}
 	 * @param args Arguments to put into the {}'s, in order.
 	 */
 	void error(Messager log, String pattern, Object... args) {
-		log.printMessage(Diagnostic.Kind.ERROR, TextUtil.format(pattern, args));
+		log.printMessage(Kind.ERROR, TextUtil.format(pattern, args));
 	}
-	
+
 	/**
 	 * Match up a causal Element (Basically the compiler representation of a
 	 * class to be compiled) w/ the Class name that will be registered in
 	 * a service registry.
-	 * 
+	 *
 	 * When the AnnotationProcessor writes a new file to the Filer, it wants
 	 * a causal Element to associate with it, apparently this info could be
 	 * used for reporting or something.
@@ -284,12 +286,44 @@ public class AndHowCompileProcessor extends AbstractProcessor {
 	protected static class CauseEffect {
 		String fullClassName;
 		Element causeElement;
-		
+
 		public CauseEffect(String fullClassName, Element causeElement) {
 			this.fullClassName = fullClassName;
 			this.causeElement = causeElement;
 		}
 	}
 
+	/**
+	 * With the introduction of IntelliJ Idea 2020.3 release the ProcessingEnvironment
+	 * is not of type com.sun.tools.javac.processing.JavacProcessingEnvironment
+	 * but java.lang.reflect.Proxy.
+	 * The com.sun.source.util.Trees.instance() throws an IllegalArgumentException when the proxied processingEnv is passed.
+	 *
+	 * @author Vicky Ronnen (vicky.ronnen@upcmail.nl or v.ronnen@gmail.com)
+	 * @link https://youtrack.jetbrains.com/issue/IDEA-256707
+	 * @param processingEnv possible proxied
+	 * @return ProcessingEnvironment unwrapped from the proxy if proxied or the original processingEnv
+	 */
+	private ProcessingEnvironment unwrap(ProcessingEnvironment processingEnv) {
+		if (Proxy.isProxyClass(processingEnv.getClass())) {
+			InvocationHandler invocationHandler = Proxy.getInvocationHandler(processingEnv);
+			try {
+				Field field = invocationHandler.getClass().getDeclaredField("val$delegateTo");
+				field.setAccessible(true);
+				Object o = field.get(invocationHandler);
+				if (o instanceof ProcessingEnvironment) {
+					return (ProcessingEnvironment) o;
+				} else {
+					processingEnv.getMessager().printMessage(Kind.ERROR, "got " + o.getClass() + " expected instanceof com.sun.tools.javac.processing.JavacProcessingEnvironment");
+					return null;
+				}
+			} catch (NoSuchFieldException | IllegalAccessException e) {
+				processingEnv.getMessager().printMessage(Kind.ERROR, e.getMessage());
+				return null;
+			}
+		} else {
+			return processingEnv;
+		}
+	}
 
 }
