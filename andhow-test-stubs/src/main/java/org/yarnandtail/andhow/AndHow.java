155e48aa9162f60cdaa0f1455e35fcbce61f5ccd
Eric Everman <eeverman@gmail.com>
2021-08-06 13:59:09 -0500

    Add some backdoor methods to the AndHow stub and add tests

--- andhow-test-stubs/src/main/java/org/yarnandtail/andhow/AndHow.java
+++ andhow-test-stubs/src/main/java/org/yarnandtail/andhow/AndHow.java
@@ -35,8 +35,10 @@ public class AndHow {
 	 * @return 
 	 */
 	public static AndHow instance() {
-		if (singleInstance == null || singleInstance.core == null) {
-			fullyInitialize();
+		if (singleInstance == null) {
+			_fullyInitialize();
+		} else if (singleInstance.core == null) {
+			singleInstance.core = new AndHowCore();
 		}
 		return singleInstance;
 	}
@@ -46,11 +48,15 @@ public class AndHow {
 
 	}
 
+	//
+	// All these methods starting with '_' are not part of the object being stubbed,
+	// they are just backdoor methods to make testing setup and verification easier.
+
 	/**
 	 * Method to initialize the singleton for testing, which just destroys everything and
 	 * replaces it w/ new instances.
 	 */
-	public static void fullyInitialize() {
+	public static void _fullyInitialize() {
 		singleInstance = new AndHow(null);
 		initialization = new Initialization();
 		initializing = false;
@@ -58,4 +64,93 @@ public class AndHow {
 		configLocator = null;
 		findingConfig.set(false);
 	}
+	
+	/**
+	 * Method to fully destroy the singleton after testing so other usages start clean.
+	 */
+	public static void _fullyDestroy() {
+		singleInstance = null;
+		initialization = null;
+		initializing = false;
+		inProcessConfig = null;
+		configLocator = null;
+		findingConfig.set(false);
+	}
+
+	/**
+	 * Backdoor test getter for the AndHow singleInstance w/o no intialization / side effects.
+	 *
+	 * @return The current value, which may be null.
+	 */
+	public static AndHow _getSingleInstance() {
+		return singleInstance;
+	}
+
+	/**
+	 * Backdoor test getter for the AndHow initialization.
+	 *
+	 * @return The current value, which may be null.
+	 */
+	public static AndHow.Initialization _getInitialization() {
+		return initialization;
+	}
+
+	/**
+	 * Backdoor test getter for the AndHow initializing flag.
+	 *
+	 * @return The current value, which should only be true or false.
+	 */
+	public static Boolean _getInitializing() {
+		return initializing;
+	}
+
+	/**
+	 * Backdoor test getter for the AndHow inProcessConfig.
+	 *
+	 * @return The current value, which may be null.
+	 */
+	public static AndHowConfiguration<? extends AndHowConfiguration> _getInProcessConfig() {
+		return inProcessConfig;
+	}
+
+	/**
+	 * Backdoor test getter for the AndHow configLocator.
+	 *
+	 * @return The current value, which may be null.
+	 */
+	public static UnaryOperator<AndHowConfiguration<? extends AndHowConfiguration>>
+			_getConfigLocator() {
+		return configLocator;
+	}
+
+	/**
+	 * Backdoor test getter for the AndHow findingConfig.
+	 *
+	 * @return The current value, which may be null.
+	 */
+	public static ThreadLocal<Boolean> _getFindingConfig() {
+		return findingConfig;
+	}
+	
+	/**
+	 * Backdoor test getter for the AndHowCore instance field.
+	 * 
+	 * @return The current value, which may be null.
+	 */
+	public AndHowCore _getAndHowCore() {
+		return core;
+	}
+	
+	/**
+	 * Backdoor test setter for the AndHowCore instance field.
+	 * 
+	 * @param newCore
+	 * @return The old core.
+	 */
+	public AndHowCore _setAndHowCore(AndHowCore newCore) {
+		AndHowCore old = core;
+		core = newCore;
+		return old;
+	}
+
 }
