37a0e81a64ea9f4095582d174a803dba75f3340f
vinayakkumargupta <vinayakkr9304@gmail.com>
2021-08-20 19:33:24 +0530

    FixedValueLoader.setPropertyValues needs to be fully deprecated

--- andhow-core/src/main/java/org/yarnandtail/andhow/load/FixedValueLoader.java
+++ andhow-core/src/main/java/org/yarnandtail/andhow/load/FixedValueLoader.java
@@ -17,20 +17,20 @@ import org.yarnandtail.andhow.api.*;
  * @author eeverman
  */
 public class FixedValueLoader extends BaseLoader implements ReadLoader {
-
+    @Deprecated
 	protected boolean unknownPropertyAProblem = true;
-	
+	@Deprecated
 	protected List<PropertyValue> values = new ArrayList();
-			
+	@Deprecated
 	public FixedValueLoader() {
 	}
-	
+	@Deprecated
 	public void setPropertyValues(List<PropertyValue> values) {
 		if (values != null) {
 			this.values.addAll(values);
 		}
 	}
-	
+	@Deprecated
 	public void setPropertyValues(PropertyValue... values) {
 		if (values != null && values.length > 0) {
 			this.values.addAll(Arrays.asList(values));
