d3308bc67d691adb7b44faed5e8db19be3fc4575
Eric Everman <eeverman@gmail.com>
2021-10-30 21:37:20 -0500

    latest test versions

--- andhow-core/src/test/java/org/yarnandtail/andhow/load/KeyValuePairLoader2Test.java
+++ andhow-core/src/test/java/org/yarnandtail/andhow/load/KeyValuePairLoader2Test.java
@@ -14,6 +14,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class KeyValuePairLoader2Test extends BaseForLoaderTests {
+
+	protected static final String STRPROP_BASE = StrPropProps.class.getCanonicalName() + ".";
+
 	PropertyConfigurationMutable appDef;
 	ValidatedValuesWithContextMutable appValuesBuilder;
 
@@ -36,123 +39,181 @@ public class KeyValuePairLoader2Test extends BaseForLoaderTests {
 		List<NameAndProperty> naps = AndHowUtil.getProperties(clazz);
 
 		for (NameAndProperty nap : naps) {
-			System.out.println("assertEquals(\"" +
-					result.getExplicitValue(nap.property) +
-					"\", result.getExplicitValue(" + clazz.getSimpleName() + "." + nap.fieldName + "));");
+			Object val = result.getExplicitValue(nap.property);
+
+			if (val != null) {
+				String strVal = val.toString();
+				strVal = strVal.replace("\"", "\\\"");
+				strVal = strVal.replace("\t", "\\t");
+				strVal = strVal.replace("\n", "\\n");
+
+				System.out.println("assertEquals(\"" + strVal +
+						"\", result.getExplicitValue(" + clazz.getSimpleName() + "." + nap.fieldName + "));");
+
+			} else {
+				System.out.println("assertNull(" +
+						"result.getExplicitValue(" + clazz.getSimpleName() + "." + nap.fieldName + "));");
+
+			}
 
 		}
 	}
-
 
 	@Test
 	public void setEveryValue1() throws IllegalAccessException {
 		List<String> args = new ArrayList();
 
-		final String strPropBase = StrPropProps.class.getCanonicalName() + ".";
-
 		// Null OK | No Default | No Validations
-		args.add(strPropBase + "PROP_0" + "=" + "  two words  ");
+		args.add(STRPROP_BASE + "PROP_0" + "=" + "  two \nwords  ");
 		// Null OK | Has Default | No Validations
-		args.add(strPropBase + "PROP_10" + "=" + "  \" words in space \"  ");
+		args.add(STRPROP_BASE + "PROP_10" + "=" + "  \" words in space \"  ");
 
 		//
 		// Null OK | No Default | Has Validations
-		args.add("StrPropProps.PROP_20" + "=" + "  StaRing  ");
-		args.add(strPropBase + "PROP_21" + "=" + " \t starInG \t ");
-		args.add(strPropBase + "PROP_22" + "=" + "  baddadcad  ");
-		args.add(strPropBase + "PROP_23" + "=" + "  ab  ");
-		args.add(strPropBase + "PROP_24" + "=" + "  \taB\t  ");
-		args.add(strPropBase + "PROP_25" + "=" + "\t  preorder  \t");
-		args.add(strPropBase + "PROP_26" + "=" + "  pReorder  ");
+		args.add("StrPropProps.PROP_20" + "=" + "  StaR\tT ing  ");
+		args.add(STRPROP_BASE + "PROP_21" + "=" + "\t\"star\tInG\"\t");
+		args.add(STRPROP_BASE + "PROP_22" + "=" + " \"a b\" ");
+		args.add(STRPROP_BASE + "PROP_23" + "=" + " \"A B\" ");
 
 		//
 		// Null OK | Has Default | Has Validations
-		args.add("StrPropProps.PROP_30" + "=" + "StaRing");
-		args.add(strPropBase + "PROP_31" + "=" + "starInG");
-		args.add(strPropBase + "PROP_32" + "=" + "baddadcad");
-		args.add(strPropBase + "PROP_33" + "=" + "\"");
-		args.add(strPropBase + "PROP_34" + "=" + "\"\"");
-		args.add(strPropBase + "PROP_35" + "=" + "preorder");
-		args.add(strPropBase + "PROP_36" + "=" + "pReorder");
+		args.add("StrPropProps.PROP_30" + "=" + "  \tStaRs star inging\t  ");
+		args.add(STRPROP_BASE + "PROP_31" + "=" + "\t  \"star star iNgiNG\"  \t");
+		args.add(STRPROP_BASE + "PROP_32" + "=" + " \"a b\" ");
+		args.add(STRPROP_BASE + "PROP_33" + "=" + " \"A b\" ");
 
 		//
 		// Not Null
 
 		// Not Null | No Default | No Validations
-		args.add("StrPropProps.PROP_100" + "=" + "\"\"");
+		args.add("StrPropProps.PROP_100" + "=" + " \" \" ");
 		// Not Null | Has Default | No Validations
-		args.add(strPropBase + "PROP_110" + "=" + "\"\"");
+		args.add(STRPROP_BASE + "PROP_110" + "=" + " \"\" ");
 
 		//
 		// Not Null | No Default | Has Validations
-		args.add("StrPropProps.PROP_120" + "=" + "StaRing");
-		args.add(strPropBase + "PROP_121" + "=" + "starInG");
-		args.add(strPropBase + "PROP_122" + "=" + "baddadcad");
-		args.add(strPropBase + "PROP_123" + "=" + "'");
-		args.add(strPropBase + "PROP_124" + "=" + "aB");
-		args.add(strPropBase + "PROP_125" + "=" + "preorder");
-		args.add(strPropBase + "PROP_126" + "=" + "pReorder");
+		args.add("StrPropProps.PROP_120" + "=" + "\t  StaR\tstar\ting  \t");
+		args.add(STRPROP_BASE + "PROP_121" + "=" + "\"star\tstar\tNG\"");
+		args.add(STRPROP_BASE + "PROP_122" + "=" + "\t\b\n\r\f \"a b\" \t\b\n\r\f");
+		args.add(STRPROP_BASE + "PROP_123" + "=" + " \t\b\n\r\f\"a B\"\t\b\n\r\f ");
+
 
 		//
-		// These won't need much testing since they have default
 		// Not Null | Has Default | Has Validations
-		args.add("StrPropProps.PROP_130" + "=" + "StaRing");
-		args.add(strPropBase + "PROP_131" + "=" + "starInG");
-		args.add(strPropBase + "PROP_132" + "=" + "baddadcad");
-		args.add(strPropBase + "PROP_133" + "=" + "ab");
-		args.add(strPropBase + "PROP_134" + "=" + "\"\"");
-		args.add(strPropBase + "PROP_135" + "=" + "preorder");
-		args.add(strPropBase + "PROP_136" + "=" + "pReorder");
+		args.add("StrPropProps.PROP_130" + "=" + "\t\b\n\r\f StaRs star inging \t\b\n\r\f ");
+		args.add(STRPROP_BASE + "PROP_131" + "=" + " \t\b\n\r\f star star iNgiNG \t\b\n\r\f ");
+		args.add(STRPROP_BASE + "PROP_132" + "=" + " \"a b\" ");
+		args.add(STRPROP_BASE + "PROP_133" + "=" + " \"A b\" ");
 
 		//
 		// Special Trimmers and Types
 		args.add("StrPropProps.PROP_200" + "=" + "  \" space_n_quotes \" ");
-		args.add(strPropBase + "PROP_210" + "=" + "upperCaseMe");
+		args.add(STRPROP_BASE + "PROP_210" + "=" + " \" upperCaseMe \" ");
 
 
 		//
 		KeyValuePairLoader kvpl = new KeyValuePairLoader();
 		kvpl.setKeyValuePairs(args);
 		LoaderValues result = kvpl.load(appDef, appValuesBuilder);
-		printValues(result, StrPropProps.class);
+
+		//printValues(result, StrPropProps.class);
+		assertEveryValue1(result);
+	}
+
+	@Test
+	public void setEveryValue2() throws IllegalAccessException {
+		List<String> args = new ArrayList();
+
+		// Null OK | No Default | No Validations
+		args.add(STRPROP_BASE + "PROP_0" + "=" + "  two \nwords  ");
+		// Null OK | Has Default | No Validations
+		args.add(STRPROP_BASE + "PROP_10" + "=" + "  \" words in space \"  ");
+
+		//
+		// Null OK | No Default | Has Validations
+		args.add("StrPropProps.PROP_20" + "=" + "  StaR\tT ing  ");
+		args.add(STRPROP_BASE + "PROP_21" + "=" + "\t\"star\tInG\"\t");
+		args.add(STRPROP_BASE + "PROP_22" + "=" + " \"a b\" ");
+		args.add(STRPROP_BASE + "PROP_23" + "=" + " \"A B\" ");
+
+		//
+		// Null OK | Has Default | Has Validations
+		args.add("StrPropProps.PROP_30" + "=" + "  \tStaRs star inging\t  ");
+		args.add(STRPROP_BASE + "PROP_31" + "=" + "\t  \"star star iNgiNG\"  \t");
+		args.add(STRPROP_BASE + "PROP_32" + "=" + " \"a b\" ");
+		args.add(STRPROP_BASE + "PROP_33" + "=" + " \"A b\" ");
+
+		//
+		// Not Null
+
+		// Not Null | No Default | No Validations
+		args.add("StrPropProps.PROP_100" + "=" + " \" \" ");
+		// Not Null | Has Default | No Validations
+		args.add(STRPROP_BASE + "PROP_110" + "=" + " \"\" ");
+
+		//
+		// Not Null | No Default | Has Validations
+		args.add("StrPropProps.PROP_120" + "=" + "\t  StaR\tstar\ting  \t");
+		args.add(STRPROP_BASE + "PROP_121" + "=" + "\"star\tstar\tNG\"");
+		args.add(STRPROP_BASE + "PROP_122" + "=" + "\t\b\n\r\f \"a b\" \t\b\n\r\f");
+		args.add(STRPROP_BASE + "PROP_123" + "=" + " \t\b\n\r\f\"a B\"\t\b\n\r\f ");
+
+
+		//
+		// Not Null | Has Default | Has Validations
+		args.add("StrPropProps.PROP_130" + "=" + "\t\b\n\r\f StaRs star inging \t\b\n\r\f ");
+		args.add(STRPROP_BASE + "PROP_131" + "=" + " \t\b\n\r\f star star iNgiNG \t\b\n\r\f ");
+		args.add(STRPROP_BASE + "PROP_132" + "=" + " \"a b\" ");
+		args.add(STRPROP_BASE + "PROP_133" + "=" + " \"A b\" ");
+
+		//
+		// Special Trimmers and Types
+		args.add("StrPropProps.PROP_200" + "=" + "  \" space_n_quotes \" ");
+		args.add(STRPROP_BASE + "PROP_210" + "=" + " \" upperCaseMe \" ");
+
+
+		//
+		KeyValuePairLoader kvpl = new KeyValuePairLoader();
+		kvpl.setKeyValuePairs(args);
+		LoaderValues result = kvpl.load(appDef, appValuesBuilder);
+
+		//printValues(result, StrPropProps.class);
+		assertEveryValue1(result);
 	}
 
 
+
+
+
 	public void assertEveryValue1(LoaderValues result) {
-		assertEquals("two words", result.getExplicitValue(StrPropProps.PROP_0));
+		assertEquals("two \nwords", result.getExplicitValue(StrPropProps.PROP_0));
 		assertEquals(" words in space ", result.getExplicitValue(StrPropProps.PROP_10));
-		assertEquals("StaRing", result.getExplicitValue(StrPropProps.PROP_20));
-		assertEquals("starInG", result.getExplicitValue(StrPropProps.PROP_21));
-		assertEquals("baddadcad", result.getExplicitValue(StrPropProps.PROP_22));
-		assertEquals("ab", result.getExplicitValue(StrPropProps.PROP_23));
-		assertEquals("aB", result.getExplicitValue(StrPropProps.PROP_24));
-		assertEquals("preorder", result.getExplicitValue(StrPropProps.PROP_25));
-		assertEquals("pReorder", result.getExplicitValue(StrPropProps.PROP_26));
-		assertEquals("StaRing", result.getExplicitValue(StrPropProps.PROP_30));
-		assertEquals("starInG", result.getExplicitValue(StrPropProps.PROP_31));
-		assertEquals("baddadcad", result.getExplicitValue(StrPropProps.PROP_32));
-		assertEquals("\"", result.getExplicitValue(StrPropProps.PROP_33));
-		assertEquals("", result.getExplicitValue(StrPropProps.PROP_34));
-		assertEquals("preorder", result.getExplicitValue(StrPropProps.PROP_35));
-		assertEquals("pReorder", result.getExplicitValue(StrPropProps.PROP_36));
-		assertEquals("", result.getExplicitValue(StrPropProps.PROP_100));
+
+		assertEquals("StaR\tT ing", result.getExplicitValue(StrPropProps.PROP_20));
+		assertEquals("star\tInG", result.getExplicitValue(StrPropProps.PROP_21));
+		assertEquals("a b", result.getExplicitValue(StrPropProps.PROP_22));
+		assertEquals("A B", result.getExplicitValue(StrPropProps.PROP_23));
+
+		assertEquals("StaRs star inging", result.getExplicitValue(StrPropProps.PROP_30));
+		assertEquals("star star iNgiNG", result.getExplicitValue(StrPropProps.PROP_31));
+		assertEquals("a b", result.getExplicitValue(StrPropProps.PROP_32));
+		assertEquals("A b", result.getExplicitValue(StrPropProps.PROP_33));
+
+		assertEquals(" ", result.getExplicitValue(StrPropProps.PROP_100));
 		assertEquals("", result.getExplicitValue(StrPropProps.PROP_110));
-		assertEquals("StaRing", result.getExplicitValue(StrPropProps.PROP_120));
-		assertEquals("starInG", result.getExplicitValue(StrPropProps.PROP_121));
-		assertEquals("baddadcad", result.getExplicitValue(StrPropProps.PROP_122));
-		assertEquals("'", result.getExplicitValue(StrPropProps.PROP_123));
-		assertEquals("aB", result.getExplicitValue(StrPropProps.PROP_124));
-		assertEquals("preorder", result.getExplicitValue(StrPropProps.PROP_125));
-		assertEquals("pReorder", result.getExplicitValue(StrPropProps.PROP_126));
-		assertEquals("StaRing", result.getExplicitValue(StrPropProps.PROP_130));
-		assertEquals("starInG", result.getExplicitValue(StrPropProps.PROP_131));
-		assertEquals("baddadcad", result.getExplicitValue(StrPropProps.PROP_132));
-		assertEquals("ab", result.getExplicitValue(StrPropProps.PROP_133));
-		assertEquals("", result.getExplicitValue(StrPropProps.PROP_134));
-		assertEquals("preorder", result.getExplicitValue(StrPropProps.PROP_135));
-		assertEquals("pReorder", result.getExplicitValue(StrPropProps.PROP_136));
+
+		assertEquals("StaR\tstar\ting", result.getExplicitValue(StrPropProps.PROP_120));
+		assertEquals("star\tstar\tNG", result.getExplicitValue(StrPropProps.PROP_121));
+		assertEquals("a b", result.getExplicitValue(StrPropProps.PROP_122));
+		assertEquals("a B", result.getExplicitValue(StrPropProps.PROP_123));
+
+		assertEquals("StaRs star inging", result.getExplicitValue(StrPropProps.PROP_130));
+		assertEquals("star star iNgiNG", result.getExplicitValue(StrPropProps.PROP_131));
+		assertEquals("a b", result.getExplicitValue(StrPropProps.PROP_132));
+		assertEquals("A b", result.getExplicitValue(StrPropProps.PROP_133));
+
 		assertEquals("\" space_n_quotes \"", result.getExplicitValue(StrPropProps.PROP_200));
-		assertEquals("UPPERCASEME", result.getExplicitValue(StrPropProps.PROP_210));
+		assertEquals(" UPPERCASEME ", result.getExplicitValue(StrPropProps.PROP_210));
 	}
 
 }
--- andhow-core/src/test/java/org/yarnandtail/andhow/ztest/StrPropProps.java
+++ andhow-core/src/test/java/org/yarnandtail/andhow/ztest/StrPropProps.java
@@ -1,25 +1,23 @@
 package org.yarnandtail.andhow.ztest;
 
 import org.yarnandtail.andhow.api.ParsingException;
-import org.yarnandtail.andhow.api.ValueType;
 import org.yarnandtail.andhow.property.StrProp;
 import org.yarnandtail.andhow.property.TrimToNullTrimmer;
 import org.yarnandtail.andhow.valuetype.BaseValueType;
-import org.yarnandtail.andhow.valuetype.StrType;
 
 /*
- *            Has Default | Has Validations
- *  Null OK		N	N	0	Alias for 1 in each set
- *  Null OK		Y	N	10
- *  Null OK		N	Y	20
- *  Null OK		Y	Y	30
- *  Not Null		N	N	100
- *  Not Null		Y	N	110
- *  Not Null		N	Y	120
- *  Not Null		Y	Y	130
- *
- *  Alt Trimmer 200
- *  Alt Type 210
+ *  | Series   |NullOK?| Default | Valid? |
+ *  | 0  - 9   |   Y   |		N	   |    N	  |
+ *  | 10 - 19  |   Y   |    Y    |    N	  |
+ *  | 20 - 29  |   Y   |    N    |    Y   |
+ *  | 30 - 39  |   Y   |    Y    |    Y   |
+ *  | 100-109  |   N   |    N    |    N   |
+ *  | 110-119  |   N   |    Y    |    N   |
+ *  | 120-129  |   N   |    N    |    Y   |
+ *  | 130-139  |   N   |    Y    |    Y   |
+ *  | 200      | Alt. Trimmer             |
+ *  | 210      | Alt. Type                |
+ *  In Alias for 1st in each set
  *
  * Special combinations / considerations:
  * Default w/ quote
@@ -32,27 +30,29 @@ public class StrPropProps {
 
 	//
 	// Null OK | Has Default | No Validations
-	public static final StrProp PROP_10 = StrProp.builder().defaultValue("prop10").build();
+	public static final StrProp PROP_10 = StrProp.builder().defaultValue(" prop 10 ").build();
 
 	//
 	// Null OK | No Default | Has Validations
-	public static final StrProp PROP_20 = StrProp.builder().startsWithIgnoringCase("star").endsWith("ing").aliasIn("StrPropProps.PROP_20").build();
-	public static final StrProp PROP_21 = StrProp.builder().startsWith("star").endsWithIgnoringCase("ing").build();
-	public static final StrProp PROP_22 = StrProp.builder().matches("[abcd]+").build();
-	public static final StrProp PROP_23 = StrProp.builder().oneOf("ab", "cd", "\"", "'").build();
-	public static final StrProp PROP_24 = StrProp.builder().oneOfIgnoringCase("ab", "cd", "").build();
-	public static final StrProp PROP_25 = StrProp.builder().startsWith("pre").build();
-	public static final StrProp PROP_26 = StrProp.builder().startsWithIgnoringCase("pre").build();
+	public static final StrProp PROP_20 = StrProp.builder().startsWithIgnoringCase("star").endsWith("ing")
+			.matches("[starSTARing \t]+").aliasIn("StrPropProps.PROP_20").build();
+	public static final StrProp PROP_21 = StrProp.builder().startsWith("star").endsWithIgnoringCase("ing")
+			.matches("[staringING \t]+").build();
+	public static final StrProp PROP_22 = StrProp.builder().oneOf(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
+	public static final StrProp PROP_23 = StrProp.builder().oneOfIgnoringCase(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
+
 
 	//
 	// Null OK | Has Default | Has Validations
-	public static final StrProp PROP_30 = StrProp.builder().defaultValue("Starting").startsWithIgnoringCase("star").endsWith("ing").aliasIn("StrPropProps.PROP_30").build();
-	public static final StrProp PROP_31 = StrProp.builder().defaultValue("startInG").startsWith("star").endsWithIgnoringCase("ing").build();
-	public static final StrProp PROP_32 = StrProp.builder().defaultValue("dab").matches("[abcd]+").build();
-	public static final StrProp PROP_33 = StrProp.builder().defaultValue("cd").oneOf("ab", "cd", "\"", "'").build();
-	public static final StrProp PROP_34 = StrProp.builder().defaultValue("Cd").oneOfIgnoringCase("ab", "cd", "").build();
-	public static final StrProp PROP_35 = StrProp.builder().defaultValue("preorder").startsWith("pre").build();
-	public static final StrProp PROP_36 = StrProp.builder().defaultValue("Preorder").startsWithIgnoringCase("pre").build();
+	public static final StrProp PROP_30 = StrProp.builder().defaultValue("Star t ing")
+			.startsWithIgnoringCase("star").endsWith("ing").matches("[starSTARing \t]+")
+			.aliasIn("StrPropProps.PROP_30").build();
+	public static final StrProp PROP_31 = StrProp.builder().defaultValue("star t InG")
+			.startsWith("star").endsWithIgnoringCase("ing").matches("[staringING \t]+").build();
+	public static final StrProp PROP_32 = StrProp.builder().defaultValue(" \"a b\" ")
+			.oneOf(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
+	public static final StrProp PROP_33 = StrProp.builder().defaultValue(" \"A B\" ")
+			.oneOfIgnoringCase(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
 
 	//
 	// Not Null
@@ -63,32 +63,37 @@ public class StrPropProps {
 
 	//
 	// Not Null | Has Default | No Validations
-	public static final StrProp PROP_110 = StrProp.builder().notNull().defaultValue("prop110").build();
+	public static final StrProp PROP_110 = StrProp.builder().notNull().defaultValue(" \"prop 110\" ").build();
 
 	//
 	// Not Null | No Default | Has Validations
-	public static final StrProp PROP_120 = StrProp.builder().notNull().startsWithIgnoringCase("star").endsWith("ing").aliasIn("StrPropProps.PROP_120").build();
-	public static final StrProp PROP_121 = StrProp.builder().notNull().startsWith("star").endsWithIgnoringCase("ing").build();
-	public static final StrProp PROP_122 = StrProp.builder().notNull().matches("[abcd]+").build();
-	public static final StrProp PROP_123 = StrProp.builder().notNull().oneOf("ab", "cd", "\"", "'").build();
-	public static final StrProp PROP_124 = StrProp.builder().notNull().oneOfIgnoringCase("ab", "cd", "").build();
-	public static final StrProp PROP_125 = StrProp.builder().notNull().startsWith("pre").build();
-	public static final StrProp PROP_126 = StrProp.builder().notNull().startsWithIgnoringCase("pre").build();
+	public static final StrProp PROP_120 = StrProp.builder().notNull()
+			.startsWithIgnoringCase("star").endsWith("ing").matches("[starSTARing \t]+").aliasIn("StrPropProps.PROP_120").build();
+	public static final StrProp PROP_121 = StrProp.builder().notNull()
+			.startsWith("star").endsWithIgnoringCase("ing").matches("[staringING \t]+").build();
+	public static final StrProp PROP_122 = StrProp.builder().notNull()
+			.oneOf(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
+	public static final StrProp PROP_123 = StrProp.builder().notNull()
+			.oneOfIgnoringCase(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
+
 
 	//
 	// These won't need much testing since they have default
 	// Not Null | Has Default | Has Validations
-	public static final StrProp PROP_130 = StrProp.builder().notNull().defaultValue("Starting").startsWithIgnoringCase("star").endsWith("ing").aliasIn("StrPropProps.PROP_130").build();
-	public static final StrProp PROP_131 = StrProp.builder().notNull().defaultValue("startInG").startsWith("star").endsWithIgnoringCase("ing").build();
-	public static final StrProp PROP_132 = StrProp.builder().notNull().defaultValue("dab").matches("[abcd]+").build();
-	public static final StrProp PROP_133 = StrProp.builder().notNull().defaultValue("cd").oneOf("ab", "cd", "\"", "'").build();
-	public static final StrProp PROP_134 = StrProp.builder().notNull().defaultValue("Cd").oneOfIgnoringCase("ab", "cd", "").build();
-	public static final StrProp PROP_135 = StrProp.builder().notNull().defaultValue("preorder").startsWith("pre").build();
-	public static final StrProp PROP_136 = StrProp.builder().notNull().defaultValue("Preorder").startsWithIgnoringCase("pre").build();
+	public static final StrProp PROP_130 = StrProp.builder().notNull().defaultValue("Star t ing")
+			.startsWithIgnoringCase("star").endsWith("ing").matches("[starSTARing \t]+")
+			.aliasIn("StrPropProps.PROP_130").build();
+	public static final StrProp PROP_131 = StrProp.builder().notNull().defaultValue("star t InG")
+			.startsWith("star").endsWithIgnoringCase("ing").matches("[staringING \t]+").build();
+	public static final StrProp PROP_132 = StrProp.builder().notNull().defaultValue(" \"a b\" ")
+			.oneOf(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
+	public static final StrProp PROP_133 = StrProp.builder().notNull().defaultValue(" \"A B\" ")
+			.oneOfIgnoringCase(" \"a b\" ", " cd ", "\"\"", " \"' ").build();
 
 	//
 	// Special Trimmers and Types
-	public static final StrProp PROP_200 = StrProp.builder().trimmer(TrimToNullTrimmer.instance()).aliasIn("StrPropProps.PROP_200").build();
+	public static final StrProp PROP_200 = StrProp.builder().trimmer(TrimToNullTrimmer.instance())
+			.aliasIn("StrPropProps.PROP_200").build();
 	public static final StrProp PROP_210 = StrProp.builder().valueType(UpperCaseParser.instance())
 			.matches("[A-Z]+").aliasIn("StrPropProps.PROP_210").build();
 
