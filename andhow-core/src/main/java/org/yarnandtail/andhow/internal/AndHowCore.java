3a310a8f52562e5625b4e3794e11b935125b4ed0
Eric <24287468+eeverman@users.noreply.github.com>
2021-11-16 16:58:01 -0600

    Issue607 using an unregistered property should cause error (#690)
    * Initial implementation
    * small change in AndHowCore prep for moving a method out to a util class (but not fully completed in the PR)
    * Moved exception class to internal
    * Added a missed line of coverage

--- andhow-core/src/main/java/org/yarnandtail/andhow/internal/AndHowCore.java
+++ andhow-core/src/main/java/org/yarnandtail/andhow/internal/AndHowCore.java
@@ -1,30 +1,27 @@
 package org.yarnandtail.andhow.internal;
 
-import java.io.*;
-
-import org.yarnandtail.andhow.export.PropertyExport;
-import org.yarnandtail.andhow.internal.export.ManualExportService;
-import org.yarnandtail.andhow.service.PropertyRegistrarLoader;
-import org.yarnandtail.andhow.util.AndHowUtil;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-
 import org.yarnandtail.andhow.AndHow;
 import org.yarnandtail.andhow.Options;
 import org.yarnandtail.andhow.api.*;
+import org.yarnandtail.andhow.internal.AndHowIllegalStateException.UnrecognizedPropertyException;
+import org.yarnandtail.andhow.export.PropertyExport;
+import org.yarnandtail.andhow.internal.export.ManualExportService;
 import org.yarnandtail.andhow.name.CaseInsensitiveNaming;
+import org.yarnandtail.andhow.service.PropertyRegistrarLoader;
 import org.yarnandtail.andhow.util.AndHowLog;
+import org.yarnandtail.andhow.util.AndHowUtil;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Actual central instance of AndHow's state after a successful initialization.
- *
+ * <p>
  * The AndHow class is really a proxy for this class, which allows
  * abstraction of the implementation and re-initialization during unit testing
  * by swapping out the AndHowCore implementation while maintaining the AndHow
  * singleton so references to it are stable.
- *
  */
 public class AndHowCore implements PropertyConfigurationInternal, ValidatedValues {
 	private static final AndHowLog LOG = AndHowLog.getLogger(AndHowCore.class);
@@ -41,11 +38,11 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 			List<GroupProxy> registeredGroups)
 			throws AppFatalException {
 
-		NamingStrategy namingStrategy = (naming != null)?naming:new CaseInsensitiveNaming();
+		NamingStrategy namingStrategy = (naming != null) ? naming : new CaseInsensitiveNaming();
 
 		if (loaders != null) {
 			for (Loader loader : loaders) {
-				if (! this.loaders.contains(loader)) {
+				if (!this.loaders.contains(loader)) {
 					this.loaders.add(loader);
 				} else {
 					problems.add(new ConstructionProblem.DuplicateLoader(loader));
@@ -79,9 +76,9 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 		//load values.
 		if (problems.size() > 0) {
 			AppFatalException afe = new AppFatalException(
-				"There is a problem with the basic setup of the " + AndHow.ANDHOW_INLINE_NAME + " framework. " +
-				"Since it is the framework itself that is misconfigured, no attempt was made to load values. " +
-				"See System.err, out or the log files for more details.",
+					"There is a problem with the basic setup of the " + AndHow.ANDHOW_INLINE_NAME + " framework. " +
+							"Since it is the framework itself that is misconfigured, no attempt was made to load values. " +
+							"See System.err, out or the log files for more details.",
 					problems);
 			printFailedStartupDetails(afe);
 			throw afe;
@@ -91,7 +88,7 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 
 		loadedValues = loadValues(staticConfig, problems).getValueMapWithContextImmutable();
 		doPropertyValidations(staticConfig, loadedValues, problems);
-		checkForValuesWhichMustBeNonNull(staticConfig, problems);
+		checkForValuesWhichMustBeNonNull(staticConfig, loadedValues, problems);
 
 		if (problems.size() > 0) {
 			AppFatalException afe = AndHowUtil.buildFatalException(problems);
@@ -116,13 +113,13 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 	/**
 	 * Determine the 'Groups' (classes or interfaces containing AndHow Properties) that should be in
 	 * scope of AndHow.
-	 *
+	 * <p>
 	 * In special situations (testing and exotic use cases), a non-null list of configuredGroups may
 	 * be passed in to bypass automatic discovery.  If the passed groups is null, use auto-discovery.
 	 * If non-null, use the passed list, even if empty.
 	 *
 	 * @param configuredGroups A list of groups to use instead of the normal auto-discovery.
-	 *   If null, auto-discovery is used.  If non-null (even empty) configuredGroups is used.
+	 *                         If null, auto-discovery is used.  If non-null (even empty) configuredGroups is used.
 	 * @return A list of groups that are in-scope for AndHow.  Not null.
 	 */
 	private static List<GroupProxy> findGroups(List<GroupProxy> configuredGroups) {
@@ -135,6 +132,30 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 	}
 
 	/**
+	 * Adds a NonNullPropertyProblem for each null valued Property that is required to be non-null.
+	 * <p>
+	 * All parameters are required to be non-null and will throw a null-pointer otherwise.
+	 * <p>
+	 * @param config Configuration and metadata for all known Properties
+	 * @param values Values loaded for the Properties
+	 * @param problems A list of Problems to append to
+	 */
+	public static void checkForValuesWhichMustBeNonNull(PropertyConfigurationInternal config,
+			ValidatedValues values, ProblemList<Problem> problems) {
+
+		for (Property<?> prop : config.getProperties()) {
+			if (prop.isNonNullRequired()) {
+				if (values.getValue(prop) == null) {
+
+					problems.add(new RequirementProblem.NonNullPropertyProblem(
+							config.getGroupForProperty(prop).getProxiedGroup(), prop));
+				}
+			}
+		}
+
+	}
+
+	/**
 	 * Prints failed startup details to System.err
 	 *
 	 * @param afe
@@ -142,7 +163,7 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 	private void printFailedStartupDetails(AppFatalException afe) {
 
 		File sampleDir = ReportGenerator.printConfigSamples(staticConfig, loaders, true);
-		String sampleDirStr = (sampleDir != null)?sampleDir.getAbsolutePath():"";
+		String sampleDirStr = (sampleDir != null) ? sampleDir.getAbsolutePath() : "";
 		afe.setSampleDirectory(sampleDirStr);
 
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -154,7 +175,7 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 			//Add separator prefix to prevent log prefixes from indenting 1st line
 			System.err.println(System.lineSeparator() + message);
 		} catch (UnsupportedEncodingException ex) {
-			ReportGenerator.printProblems(System.err, afe, staticConfig);	//shouldn't happen
+			ReportGenerator.printProblems(System.err, afe, staticConfig);  //shouldn't happen
 		}
 
 	}
@@ -167,21 +188,6 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 		} else {
 			throw new IllegalArgumentException("Cannot export a null list of classes");
 		}
-	}
-
-	@Override
-	public boolean isExplicitlySet(Property<?> prop) {
-		return loadedValues.isExplicitlySet(prop);
-	}
-
-	@Override
-	public <T> T getExplicitValue(Property<T> prop) {
-		return loadedValues.getExplicitValue(prop);
-	}
-
-	@Override
-	public <T> T getValue(Property<T> prop) {
-		return loadedValues.getValue(prop);
 	}
 
 	//TODO:  Shouldn't this be stateless and pass in the loader list?
@@ -202,13 +208,13 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 	/**
 	 * Validates all Property values.
 	 *
-	 * @param config Needed bc validation is done while construction is
-	 *	not complete, thus the as-is definition is needed prior to it being complete.
+	 * @param config       Needed bc validation is done while construction is
+	 *                     not complete, thus the as-is definition is needed prior to it being complete.
 	 * @param loadedValues The values to be validated.
-	 * @param problems Add any new problems to this list
+	 * @param problems     Add any new problems to this list
 	 */
 	private void doPropertyValidations(PropertyConfigurationInternal config,
-                                       ValidatedValuesWithContext loadedValues, ProblemList<Problem> problems) {
+			ValidatedValuesWithContext loadedValues, ProblemList<Problem> problems) {
 
 		for (LoaderValues lvs : loadedValues.getAllLoaderValues()) {
 			for (ValidatedValue pv : lvs.getValues()) {
@@ -220,20 +226,20 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 	/**
 	 * Does validation on a single Property value as loaded by a single loader.
 	 *
-	 * @param <T> The shared type of the Property and Value.
-	 * @param config Needed bc validation is done while construction is
-	 *	not complete, thus the as-is definition is needed prior to it being complete.
-	 * @param loader The loader used to load the value, for context when creating a Problem.
-	 * @param problems Add any new problems to this list
+	 * @param <T>          The shared type of the Property and Value.
+	 * @param config       Needed bc validation is done while construction is
+	 *                     not complete, thus the as-is definition is needed prior to it being complete.
+	 * @param loader       The loader used to load the value, for context when creating a Problem.
+	 * @param problems     Add any new problems to this list
 	 * @param propValue<T> The Property and its value, both of type 'T'.
 	 */
 	private <T> void doPropertyValidation(PropertyConfigurationInternal config,
-                                          Loader loader, ProblemList<Problem> problems, ValidatedValue<T> propValue) {
+			Loader loader, ProblemList<Problem> problems, ValidatedValue<T> propValue) {
 
 		Property<T> prop = propValue.getProperty();
 
 		for (Validator<T> v : prop.getValidators()) {
-			if (! v.isValid(propValue.getValue())) {
+			if (!v.isValid(propValue.getValue())) {
 
 				ValueProblem.InvalidValueProblem problem =
 						new ValueProblem.InvalidValueProblem(loader,
@@ -247,23 +253,65 @@ public class AndHowCore implements PropertyConfigurationInternal, ValidatedValue
 	}
 
 
-	private void checkForValuesWhichMustBeNonNull(PropertyConfigurationInternal config, ProblemList<Problem> problems) {
+	//
+	// ValidatedValue Interface
 
-		for (Property<?> prop : config.getProperties()) {
-			if (prop.isNonNullRequired()) {
-				if (getValue(prop) == null) {
-
-					problems.add(new RequirementProblem.NonNullPropertyProblem(
-								config.getGroupForProperty(prop).getProxiedGroup(), prop));
-				}
-			}
-		}
-
+	@Override
+	public boolean isExplicitlySet(Property<?> prop) {
+		return loadedValues.isExplicitlySet(prop);
 	}
 
+	/**
+	 * The value found and loaded for this value by a Loader.
+	 * <p>
+	 * If no non-null value was found by a loader for this property, null is returned.
+	 * If the Property is not recognized, an IllegalArgumentException is thrown.
+	 * In normal usage, AndHow would not expect an unrecognized Property, so this would indicate
+	 * an unrecoverable state.
+	 *
+	 * @param <T>  The return type of the Property.
+	 * @param prop The property to get the value for
+	 * @return The value, if explicitly set, or null if not explicity set.
+	 * @throws UnrecognizedPropertyException if the Property is not recognized by AndHow.
+	 */
+	@Override
+	public <T> T getExplicitValue(Property<T> prop) throws UnrecognizedPropertyException {
+
+		T val = loadedValues.getExplicitValue(prop);
+
+		if (val == null && staticConfig.getCanonicalName(prop) == null) {
+			throw new UnrecognizedPropertyException(prop);
+		}
+
+		return val;
+	}
+
+	/**
+	 * The effective value of the Property.
+	 * <p>
+	 * The effective value is the explicitly configured value, or if that is null, the default value.
+	 * If the Property is not recognized, an IllegalArgumentException is thrown.
+	 * In normal usage, AndHow would not expect an unrecognized Property, so this would indicate
+	 * an unrecoverable state.
+	 * <p>
+	 * @param <T> The return type of the Property.
+	 * @param prop The property to get the value for.
+	 * @return The explicit value or, if no explicit, the default value.  Otherwise null.
+	 * @throws UnrecognizedPropertyException if the Property is not recognized by AndHow.
+	 */
+	@Override
+	public <T> T getValue(Property<T> prop) throws UnrecognizedPropertyException {
+		T val = getExplicitValue(prop);
+
+		if (val != null) {
+			return val;
+		} else {
+			return prop.getDefaultValue();
+		}
+	}
 
 	//
-	//ConstructionDefinition Interface
+	// ConstructionDefinition Interface
 
 	@Override
 	public List<EffectiveName> getAliases(Property<?> property) {
---
+++ andhow-core/src/test/java/org/yarnandtail/andhow/internal/AndHowCoreTest.java
@@ -0,0 +1,207 @@
+package org.yarnandtail.andhow.internal;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.yarnandtail.andhow.AndHowTestConfig;
+import org.yarnandtail.andhow.GroupExport;
+import org.yarnandtail.andhow.internal.AndHowIllegalStateException.UnrecognizedPropertyException;
+import org.yarnandtail.andhow.api.EffectiveName;
+import org.yarnandtail.andhow.api.Exporter.EXPORT_CANONICAL_NAME;
+import org.yarnandtail.andhow.api.Exporter.EXPORT_OUT_ALIASES;
+import org.yarnandtail.andhow.api.Property;
+import org.yarnandtail.andhow.export.SysPropExporter;
+import org.yarnandtail.andhow.junit5.RestoreSysPropsAfterThisTest;
+import org.yarnandtail.andhow.name.CaseInsensitiveNaming;
+import org.yarnandtail.andhow.property.IntProp;
+import org.yarnandtail.andhow.property.StrProp;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AndHowCoreTest {
+	AndHowTestConfig.AndHowTestConfigImpl config;
+	AndHowCore core;
+
+	// FYI: StrProp values copied from StrPropProps in andhow-core-integration-test
+	public interface Conf1 {
+		// Don't add to the list w/o updating tests that check for the complete list
+		StrProp PROP_0 = StrProp.builder().aliasIn("StrPropProps.PROP_0").aliasInAndOut("StrPropProps.PROP_0.io").build();
+		StrProp PROP_20 = StrProp.builder().startsWithIgnoringCase("star").endsWith("ing")
+				.matches("[starSTARing \t]+").aliasIn("StrPropProps.PROP_20").build();
+		StrProp PROP_30 = StrProp.builder().defaultValue("Star t ing")
+				.startsWithIgnoringCase("star").endsWith("ing").matches("[starSTARing \t]+")
+				.aliasIn("StrPropProps.PROP_30").build();
+		StrProp PROP_100 = StrProp.builder().notNull().aliasIn("StrPropProps.PROP_100").build();
+	}
+
+	@GroupExport(
+			exportByCanonicalName = EXPORT_CANONICAL_NAME.ALWAYS,
+			exportByOutAliases = EXPORT_OUT_ALIASES.ALWAYS, exporter = SysPropExporter.class)
+	public interface Conf2 {
+		// Don't add to the list w/o updating tests that check for the complete list
+		IntProp PROP_0 = IntProp.builder().aliasInAndOut("IntPropProps.PROP_0").build();
+		IntProp PROP_20 = IntProp.builder().greaterThanOrEqualTo(-5).lessThan(1000)
+				.aliasIn("IntPropProps.PROP_20").build();
+	}
+
+
+	@BeforeEach
+	public void init() throws Exception {
+		config = AndHowTestConfig.instance();
+
+	}
+
+	public AndHowCore initCore(AndHowTestConfig.AndHowTestConfigImpl config) {
+		core = new AndHowCore(
+				config.getNamingStrategy(),
+				config.buildLoaders(),
+				config.getRegisteredGroups());
+
+		return core;
+	}
+
+	@Test
+	@RestoreSysPropsAfterThisTest
+	public void happyPathWithAllValuesAssigned() {
+		config.addOverrideGroup(Conf1.class).addOverrideGroup(Conf2.class);
+
+		config.addFixedValue(Conf1.PROP_0, "ab");
+		config.addFixedValue(Conf1.PROP_20, "starting");
+		config.addFixedValue(Conf1.PROP_30, "STARting");
+		config.addFixedValue(Conf1.PROP_100, "anything");
+
+		config.addFixedValue(Conf2.PROP_0, 0);
+		config.addFixedValue(Conf2.PROP_20, 20);
+
+		core = initCore(config);
+
+		// Confirm export happened
+		assertEquals("0", System.getProperty(Conf2.class.getCanonicalName() + ".PROP_0"));
+		assertEquals("0", System.getProperty("IntPropProps.PROP_0"), "Export by alias should happen as well");
+		assertEquals("20", System.getProperty(Conf2.class.getCanonicalName() + ".PROP_20"));
+
+		// Confirm export did not happen for other properties / names
+		assertNull(System.getProperty(Conf1.class.getCanonicalName() + ".PROP_0"));
+		assertNull(System.getProperty("StrPropProps.PROP_0.io"));
+		assertNull(System.getProperty("IntPropProps.PROP_20"), "This alias is in-only and should not export");
+
+		// Validated Values Interface
+
+		assertEquals("ab", core.getExplicitValue(Conf1.PROP_0));
+		assertEquals("starting", core.getExplicitValue(Conf1.PROP_20));
+		assertEquals("STARting", core.getExplicitValue(Conf1.PROP_30));
+		assertEquals("anything", core.getExplicitValue(Conf1.PROP_100));
+		assertEquals(0, core.getExplicitValue(Conf2.PROP_0));
+		assertEquals(20, core.getExplicitValue(Conf2.PROP_20));
+
+		assertTrue(core.isExplicitlySet(Conf1.PROP_0));
+		assertTrue(core.isExplicitlySet(Conf1.PROP_20));
+		assertTrue(core.isExplicitlySet(Conf1.PROP_30));
+		assertTrue(core.isExplicitlySet(Conf1.PROP_100));
+		assertTrue(core.isExplicitlySet(Conf2.PROP_0));
+		assertTrue(core.isExplicitlySet(Conf2.PROP_20));
+
+
+		assertEquals("ab", core.getValue(Conf1.PROP_0));
+		assertEquals("starting", core.getValue(Conf1.PROP_20));
+		assertEquals("STARting", core.getValue(Conf1.PROP_30));
+		assertEquals("anything", core.getValue(Conf1.PROP_100));
+		assertEquals(0, core.getValue(Conf2.PROP_0));
+		assertEquals(20, core.getValue(Conf2.PROP_20));
+
+		//
+		// ConstructionDefinition Interface
+
+		// Check name and aliases on Conf1.PROP_0
+		List<EffectiveName> p0Names = core.getAliases(Conf1.PROP_0);
+		assertEquals(2, p0Names.size());
+		assertEquals(1, p0Names.stream().filter(n -> n.isOut() && n.getEffectiveOutName().equals("StrPropProps.PROP_0.io")).count());
+		assertEquals(1, p0Names.stream().filter(n -> n.isIn() && n.getEffectiveInName().equalsIgnoreCase("StrPropProps.PROP_0.io")).count());
+		assertEquals(1, p0Names.stream().filter(n -> n.isIn() && n.getEffectiveInName().equalsIgnoreCase("StrPropProps.PROP_0")).count());
+		assertEquals(Conf1.class.getCanonicalName() + ".PROP_0", core.getCanonicalName(Conf1.PROP_0));
+
+
+		assertSame(Conf1.class, core.getGroupForProperty(Conf1.PROP_0).getProxiedGroup());
+
+		List<Property<?>> conf2Props = core.getPropertiesForGroup(core.getGroupForProperty(Conf2.PROP_0));
+		assertThat(conf2Props, Matchers.containsInAnyOrder(Conf2.PROP_0, Conf2.PROP_20));
+
+		assertSame(Conf1.PROP_0, core.getProperty(Conf1.class.getCanonicalName() + ".PROP_0"));
+		assertSame(Conf1.PROP_0, core.getProperty("StrPropProps.PROP_0"));
+		assertSame(Conf1.PROP_0, core.getProperty("StrPropProps.PROP_0.io"));
+
+		List<Class<?>> groupClasses = core.getPropertyGroups().stream().map(g -> g.getProxiedGroup()).collect(Collectors.toList());
+
+		// This will contain other as well, such as AndHow Options and Loader options
+		assertThat(groupClasses, Matchers.hasItems(Conf1.class, Conf2.class));
+
+		assertTrue(core.containsUserGroups());
+
+		// This will contain others as well, such as Props from AndHow Options and Loader options
+		assertThat(core.getProperties(),
+				Matchers.hasItems(Conf1.PROP_0, Conf1.PROP_20, Conf1.PROP_30, Conf1.PROP_100,
+						Conf2.PROP_0, Conf2.PROP_20));
+
+		assertThat(core.getExportGroups().stream().map(e -> e.getGroup().getProxiedGroup()).collect(Collectors.toList()),
+				Matchers.containsInAnyOrder(Conf2.class));
+
+		assertTrue(core.getNamingStrategy() instanceof CaseInsensitiveNaming);
+	}
+
+
+	@Test
+	@RestoreSysPropsAfterThisTest
+	public void happyPathWithNonRequiredValuesUnassigned() {
+		config.addOverrideGroup(Conf1.class).addOverrideGroup(Conf2.class);
+
+		// This is the only required property
+		config.addFixedValue(Conf1.PROP_100, "anything");
+
+		core = initCore(config);
+
+		// Validated Values Interface
+
+		assertNull(core.getExplicitValue(Conf1.PROP_0));
+		assertNull(core.getExplicitValue(Conf1.PROP_20));
+		assertNull(core.getExplicitValue(Conf1.PROP_30));
+		assertEquals("anything", core.getExplicitValue(Conf1.PROP_100));
+		assertNull(core.getExplicitValue(Conf2.PROP_0));
+		assertNull(core.getExplicitValue(Conf2.PROP_20));
+
+		assertFalse(core.isExplicitlySet(Conf1.PROP_0));
+		assertFalse(core.isExplicitlySet(Conf1.PROP_20));
+		assertFalse(core.isExplicitlySet(Conf1.PROP_30));
+		assertTrue(core.isExplicitlySet(Conf1.PROP_100));
+		assertFalse(core.isExplicitlySet(Conf2.PROP_0));
+		assertFalse(core.isExplicitlySet(Conf2.PROP_20));
+
+
+		assertNull(core.getValue(Conf1.PROP_0));
+		assertNull(core.getValue(Conf1.PROP_20));
+		assertEquals("Star t ing", core.getValue(Conf1.PROP_30));
+		assertEquals("anything", core.getValue(Conf1.PROP_100));
+		assertNull(core.getValue(Conf2.PROP_0));
+		assertNull(core.getValue(Conf2.PROP_20));
+
+	}
+
+	@Test
+	@RestoreSysPropsAfterThisTest
+	public void getValueForAnUnregisteredPropertyIsAnIllegalArg() {
+		config.addOverrideGroup(Conf1.class);
+		config.addFixedValue(Conf1.PROP_100, "anything");
+		core = initCore(config);
+
+		UnrecognizedPropertyException upe = assertThrows(UnrecognizedPropertyException.class,
+				() -> core.getValue(Conf2.PROP_0)
+		);
+
+		assertSame(Conf2.PROP_0, upe.getProperty());
+
+	}
+
+}
